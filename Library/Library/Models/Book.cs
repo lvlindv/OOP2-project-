3fd57d74bd52a04b0eeb690cc8373d34112886c3
Louise Wiljander <lowi5496@user.uu.se>
2019-10-20 16:00:35 +0200

    added book class

--- Library/Library/Models/Book.cs
+++ Library/Library/Models/Book.cs
@@ -6,15 +6,28 @@ using System.Web;
 
 namespace Library.Models {
     public class Book {
-
-        public int Id { get; set; }
+        
+        /// <summary>
+        /// Properties
+        /// </summary>
+        public int BookId { get; set; } //Primärnyckel, hur läggs den till?
+        public string ISBN { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
+        public Author Author { get; set; }
+        public IEnumerable<BookCopy> BookCopy { get; set; }
+
+        public Book(string ISBN, string Title, string Description)
+        {
+            //Tilldela properties värden
+        }
 
         /// <summary>
         /// Useful for adding the book objects directly to a ListBox.
         /// </summary>
-        public override string ToString() {
-            return String.Format("[{0}] -- {1}", this.Id, this.Title);
+        public override string ToString()
+        {
+            return String.Format("[{0}] -- {1}", this.BookId, this.ISBN, this.Title, this.Description);
         }
     }
 }
