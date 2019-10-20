0a9b883acfbb3f5a2ec2090b5be45f145e17c71c
Louise Wiljander <lowi5496@user.uu.se>
2019-10-20 16:08:58 +0200

    Added classes of authors, book copies, loans and members

---
+++ Library/Library/Models/Author.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Author
+    {
+        /// <summary>
+        /// Properties
+        /// </summary>
+        public int AuthorId { get; set; } //Primary key
+        public string Name { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Author()
+        {
+            //Set values of properties
+        }
+    }
+}
---
+++ Library/Library/Models/Member.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Member
+    {
+        /// <summary>
+        /// Properties
+        /// </summary>
+        public int MemberId { get; set; } //Primary key
+        public int PersonalId { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfMembership { get; set; }
+        public IEnumerable<Loan> Loans { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Member()
+        {
+            //Set values of properties
+        }
+    }
+}
