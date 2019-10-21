fd1eaa4c5f06b091eeaf43303948ed86c180edaa
Louise Wiljander <lowi5496@user.uu.se>
2019-10-21 15:41:24 +0200

    La till namespace för EF i program

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
 
 namespace Library {
     static class Program {
