b4c439e67e8d3701eeed742ad0b5363c461e94a2
Louise Wiljander <lowi5496@user.uu.se>
2019-10-23 12:15:39 +0200

    Tog bort Remove-funktionen + lite kommentarer från AuthorRepo

--- Library/Library/Repositories/AuthorRepository.cs
+++ Library/Library/Repositories/AuthorRepository.cs
@@ -27,20 +27,12 @@ namespace Library.Repositories
             context.SaveChanges();
         }
 
-        public void Remove(Author author)
-        {
-            context.Authors.Remove(author);
-        }
-
         // Edit function from Book
         public void Edit(Author a)
         {
             // Because the object a was retrieved through the same context, we don't need to do a lookup. 
             // We can just tell the context to save any changes that happened.
             context.SaveChanges();
-            // Then why do we still pass the Book object all the way to the repository? Because the service
-            // layer doesn't know we use EF. If in the future we decide to switch EF to something else, 
-            // we won't have to change the service layer.
         }
     }
 }
